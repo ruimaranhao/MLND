54feb8c3052f5bbe349176c429cb2ecd8a8c72f7
Rui Maranhao <rui@computer.org>
2017-06-06 21:32:16 +0100

    adding .gitignore

--- capstone/code/utils.py
+++ capstone/code/utils.py
@@ -7,6 +7,9 @@ from __future__ import print_function
 import os
 import sys
 import tarfile
+import h5py
+import numpy as np
+import string
 
 from six.moves.urllib.request import urlretrieve
 from six.moves import cPickle as pickle
@@ -42,3 +45,105 @@ def maybe_download(url, filename, expected_bytes, force=False):
   else:
     print('Failed to verify ' + filename + '. Please delete and try again')
   return filename
+
+def maybe_extract(filename, force=False):
+  root = os.path.splitext(os.path.splitext(filename)[0])[0]  # remove .tar.gz
+  if os.path.isdir(root) and not force:
+    # You may override by setting force=True.
+    print('%s already present - Skipping extraction of %s.' % (root, filename))
+  else:
+    print('Extracting data for %s. This may take a while. Please wait.' % root)
+    tar = tarfile.open(filename)
+    sys.stdout.flush()
+    tar.extractall()
+    tar.close()
+  if not os.path.exists(root+'/digitStruct.mat'):
+    print("digitStruct.mat is missing")
+  return root+'/digitStruct.mat'
+
+def get_attr(c, i, attr):
+    d = c[c['digitStruct']['bbox'][i][0]][attr].value.squeeze()
+    if d.dtype == 'float64':
+        return d.reshape(-1)
+    return np.array([c[x].value for x in d]).squeeze()
+
+def get_label(c, i):
+    d = c[c['digitStruct']['name'][i][0]].value.tostring()
+    return d.replace(b'\x00', b'')
+
+def load_data(path):
+    c = h5py.File(path)
+    images = a = np.ndarray(shape=(c['digitStruct']['name'].shape[0], ), dtype='|S15')
+    labels = np.zeros((len(c['digitStruct']['bbox']), 6), dtype=float)
+    labels.fill(10)
+    tops = np.zeros((len(c['digitStruct']['bbox']), 6), dtype=float)
+    heights = np.zeros((len(c['digitStruct']['bbox']), 6), dtype=float)
+    widths = np.zeros((len(c['digitStruct']['bbox']), 6), dtype=float)
+    lefts = np.zeros((len(c['digitStruct']['bbox']), 6), dtype=float)
+    for i in range(c['digitStruct']['name'].shape[0]):
+        images[i] = get_label(c, i)
+        l = get_attr(c, i, 'label')
+        t = get_attr(c, i, 'top')
+        h = get_attr(c, i, 'height')
+        w = get_attr(c, i, 'width')
+        le = get_attr(c, i, 'left')
+
+        labels[i, :l.shape[0]] = l
+        tops[i, :t.shape[0]] = t
+        heights[i, :h.shape[0]] = h
+        widths[i, :w.shape[0]] = w
+        lefts[i, :le.shape[0]] = le
+
+        if (i % 5000 == 0):
+            print(i, "elapsed")
+
+    return labels, images, tops, heights, widths, lefts
+
+def maybe_pickle(struct, trainTuple, testTuple, extraTuple, force=False):
+    if os.path.exists(struct + '.pickle') and not force:
+      # You may override by setting force=True.
+      print('%s already present - Skipping pickling.' % struct)
+    else:
+      print('Pickling %s.' % struct + '.pickle')
+      permutation = np.random.permutation(extraTuple[1].shape[0])[:2000]
+      dataset = {
+            'train': {
+                'labels': trainTuple[0],
+                'images': trainTuple[1],
+                'tops': trainTuple[2],
+                'heights': trainTuple[3],
+                'widths': trainTuple[4],
+                'lefts': trainTuple[5],
+            },
+            'test': {
+                'labels': testTuple[0],
+                'images': testTuple[1],
+                'tops': testTuple[2],
+                'heights': testTuple[3],
+                'widths': testTuple[4],
+                'lefts': testTuple[5],
+            },
+            'extra': {
+                'labels': extraTuple[0],
+                'images': extraTuple[1],
+                'tops': extraTuple[2],
+                'heights': extraTuple[3],
+                'widths': extraTuple[4],
+                'lefts': extraTuple[5],
+            },
+            'valid': {
+                'labels': extraTuple[0][permutation],
+                'images': extraTuple[1][permutation],
+                'tops': extraTuple[2][permutation],
+                'heights': extraTuple[3][permutation],
+                'widths': extraTuple[4][permutation],
+                'lefts': extraTuple[5][permutation],
+            }
+      }
+      try:
+        with open( struct + '.pickle', 'wb') as f:
+          pickle.dump(dataset, f, pickle.HIGHEST_PROTOCOL)
+      except Exception as e:
+        print('Unable to save data to',  struct + '.pickle', ':', e)
+
+    return  struct + '.pickle'
