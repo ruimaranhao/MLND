9b8bd77831787a89fe89c0cba34372b279c49bd1
Rui Maranhao <rui@computer.org>
2017-06-06 23:09:25 +0100

    visualize data

--- capstone/code/utils.py
+++ capstone/code/utils.py
@@ -43,16 +43,17 @@ def maybe_download(url, filename, expected_bytes, force=False):
   if statinfo.st_size == expected_bytes:
     print('Found and verified', filename)
   else:
-    print('Failed to verify ' + filename + '. Please delete and try again')
+    print('Failed to verify ' + filename + '. Delete and try again')
   return filename
 
 def maybe_extract(filename, force=False):
+  """Extract file (.tar.gz)."""
   root = os.path.splitext(os.path.splitext(filename)[0])[0]  # remove .tar.gz
   if os.path.isdir(root) and not force:
     # You may override by setting force=True.
     print('%s already present - Skipping extraction of %s.' % (root, filename))
   else:
-    print('Extracting data for %s. This may take a while. Please wait.' % root)
+    print('Extracting data for %s. This may take a while. Be patient.' % root)
     tar = tarfile.open(filename)
     sys.stdout.flush()
     tar.extractall()
